66a8902037cfcefe615d733e4ff11113b6a96a77
braitcode <brightjohn489@gmail.com>
2024-08-10 20:36:17 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
  // please do not add or change anything in this file except your route which should enable you to access your production.
   return (
     <>
-      
+      <h1 className='text-ver'>Bright</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        overpass: ['Overpass', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'sans-serif']
+      },
+      colors: {
+        lightRed: 'hsl(356, 100%, 66%)',
+        veryLightRedHover: 'hsl(355, 100%, 74%)',
+        veryDarkBlue: 'hsl(208, 49%, 24%)',
+        greyishBlue: 'hsl(240, 2%, 79%)',
+        veryGrayishBlue: 'hsl(207, 13%, 34%)',
+        veryDarkBlackBlue: 'hsl(240, 10%, 16%)',
+        veryLightRed: 'hsl(13, 100%, 72%)',
+        veryDarkgrayBlue: 'hsl(237, 17%, 21%)',
+        veryDarkDesaturatedBlue: 'hsl(237, 23%, 32%)'
+      }
+    },
   },
   plugins: [],
 }
