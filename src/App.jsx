55cab856c7c90f382b6c91a03a2b05fefc6a840a
manifesto <manifestomixx@gmail.com>
2024-08-10 21:16:38 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+
+
 
 function App() {
  // please do not add or change anything in this file except your route which should enable you to access your production.
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
