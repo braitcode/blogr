1d83eb99953af82c58cf210ddf7448e2dc5e05c2
braitcode <brightjohn489@gmail.com>
2024-08-10 10:38:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ // please do not add or change anything in this file except your route which should enable you to access your production.
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
