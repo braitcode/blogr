cc12e5813efab11181b4dc04ce01bfe26f647b7e
Oluwabukola-G <amoooluwabukola@outlook.com>
2024-08-10 22:22:05 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-
+import Footer from './components/Footer'
 function App() {
  // please do not add or change anything in this file except your route which should enable you to access your production.
   return (
     <>
-      
+     <Footer/> 
     </>
   )
 }
