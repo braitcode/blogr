11f13d803e6f074ffa2c0d52c9d11ea4b1f5b1db
braitcode <brightjohn489@gmail.com>
2024-08-12 08:08:05 +0100

    future section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 
